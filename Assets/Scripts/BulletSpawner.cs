b5c85fab92a5c44adc75a7f7e88fc965a893b169
Aryaman Patra <aryamanprivate26@gmail.com>
2022-06-22 03:21:11 +0530

    Some modifications in Scripts

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -4,7 +4,7 @@ public class BulletSpawner : MonoBehaviour
 {
     public Transform Bullet;
     public Transform BulletHolder;
-    public float bulletSpeed=100f;
+    public static float bulletSpeed=100f;
     public float angleDegree = 90f;
     Transform BulletTrans;
     Rigidbody BulletRB;
@@ -20,7 +20,7 @@ public class BulletSpawner : MonoBehaviour
             AddForceAtAngle(bulletSpeed,angleDegree,BulletRB);
         }   
     }
-    public void AddForceAtAngle(float force, float angle, Rigidbody BulletRB)
+    void AddForceAtAngle(float force, float angle, Rigidbody BulletRB)
     {
         float xcomponent = Mathf.Cos(angle * Mathf.PI / 180) * force;
         float ycomponent = Mathf.Sin(angle * Mathf.PI / 180) * force;
--- Assets/Scripts/DeSpawn.cs
+++ Assets/Scripts/DeSpawn.cs
@@ -5,7 +5,10 @@ public class DeSpawn : MonoBehaviour
     
     void OnTriggerEnter(Collider cold)
     {
-        Destroy(cold.gameObject);
+        if (cold.tag == "Ground")
+            cold.transform.position = new Vector3(17.1f, 0f, 0f);
+        else
+            Destroy(cold.gameObject);
     }
 
     void OnCollisionEnter(Collision col)

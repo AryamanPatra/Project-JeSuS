0a1ce45c6661b71d173127301d4c167d8c65dc88
Aryaman Patra <aryamanprivate26@gmail.com>
2022-07-10 17:40:23 +0530

    Attempt to add trajectory to bullets - Success

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -8,15 +8,18 @@ public class BulletSpawner : MonoBehaviour
     public float angleDegree = 90f;
     Transform BulletTrans;
     Rigidbody BulletRB;
-
+    Vector3 fireLocation;
     
 
-    void Update()
+    void LateUpdate()
     {
         if (Input.GetButtonDown("Fire1")) 
         {
             BulletTrans = Instantiate(Bullet, BulletHolder.position, BulletHolder.rotation);
             BulletRB = BulletTrans.GetComponent<Rigidbody>();
+            fireLocation = transform.position;
+            // Debug.Log("FireLocation: "+fireLocation);
+            angleDegree = FindTheAngle();
             AddForceAtAngle(bulletSpeed,angleDegree,BulletRB);
         }   
     }
@@ -25,6 +28,18 @@ public class BulletSpawner : MonoBehaviour
         float xcomponent = Mathf.Cos(angle * Mathf.PI / 180) * force;
         float ycomponent = Mathf.Sin(angle * Mathf.PI / 180) * force;
         
-        BulletRB.AddForce(ycomponent, -45f, xcomponent);
+        BulletRB.AddForce(xcomponent,ycomponent,0f);
+    }
+    float FindTheAngle()
+    {
+        Vector3 P = fireLocation;
+        Vector3 Q = MousePointer.GetHitLocation();
+        float theta = Mathf.Atan((Q.y-P.y)/(Q.x-P.x)) *180/Mathf.PI;
+        if (theta>0 && (Q.y-P.y<0))
+            theta += 180;
+        else if (theta<0 && (Q.y-P.y>0))
+            theta += 180;
+        Debug.Log(theta);
+        return theta;
     }
 }
--- Assets/Scripts/MousePointer.cs
+++ Assets/Scripts/MousePointer.cs
@@ -6,16 +6,27 @@ public class MousePointer : MonoBehaviour
     [SerializeField]
     Camera mainCamera;
     [SerializeField]
-    LayerMask layerMask;
+    private LayerMask layerMask;
     [SerializeField]
     Image cursor;
+    private static Vector3 hitLocation;
 
     void Update()
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit) ){
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue , layerMask) ){
             transform.position = raycastHit.point;
         }
-        cursor.transform.position += Input.mousePosition;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            hitLocation = transform.position;
+            // Debug.Log("HitLocation: "+transform.position);
+        }
+        cursor.transform.position = Input.mousePosition; // For Game Cursor Movement
+    }
+
+    public static Vector3 GetHitLocation()
+    {
+        return hitLocation;
     }
 }

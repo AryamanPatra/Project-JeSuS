76814cc4348216db27d3f08b958cf129b106e067
Aryaman Patra <aryamanprivate26@gmail.com>
2022-08-06 11:12:27 +0530

    Trying to merge

--- Assets/Scripts/MousePointer.cs
+++ Assets/Scripts/MousePointer.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MousePointer : MonoBehaviour
 {
+<<<<<<< HEAD
     public static Vector3 pointQ;
     [SerializeField] Camera mainCamera;
     void Update()
@@ -9,5 +11,36 @@ public class MousePointer : MonoBehaviour
         transform.position = Input.mousePosition;
         Debug.Log(mainCamera.ScreenToWorldPoint(Input.mousePosition));
         pointQ = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+=======
+    [SerializeField]
+    Camera mainCamera;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    Image cursor;
+    private static Vector3 hitLocation;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue , layerMask) ){
+            transform.position = raycastHit.point;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            hitLocation = transform.position;
+            // Debug.Log("HitLocation: "+transform.position);
+        }
+        cursor.transform.position = Input.mousePosition; // For Game Cursor Movement
+    }
+
+    public static Vector3 GetHitLocation()
+    {
+        return hitLocation;
+>>>>>>> Trajectory
     }
 }

4ac2b4eaa84c81d7e7b41d2e57ec2a90a21d37d9
Aryaman Patra <aryamanprivate26@gmail.com>
2022-08-06 11:53:20 +0530

    Trying to fix older files for better merge

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -8,11 +8,7 @@ public class BulletSpawner : MonoBehaviour
     float angleRadian;
     Transform BulletTrans;
     Rigidbody BulletRB;
-<<<<<<< HEAD
     Vector3 pointP;
-=======
-    Vector3 fireLocation;
->>>>>>> Trajectory
     
 
     void LateUpdate()
@@ -25,37 +21,13 @@ public class BulletSpawner : MonoBehaviour
             Debug.Log("BSpawner: "+BulletTrans.transform.position);
             Debug.Log("Mouse: "+Input.mousePosition);
             BulletRB = BulletTrans.GetComponent<Rigidbody>();
-<<<<<<< HEAD
             AddForceAtAngle(bulletSpeed,angleRadian);
-=======
-            fireLocation = transform.position;
-            // Debug.Log("FireLocation: "+fireLocation);
-            angleDegree = FindTheAngle();
-            AddForceAtAngle(bulletSpeed,angleDegree,BulletRB);
->>>>>>> Trajectory
         }   
     }
     void AddForceAtAngle(float force, float angle)
     {
         float xcomponent = Mathf.Cos(angle) * force;
-        float ycomponent = Mathf.Sin(angle) * force;
-        
-<<<<<<< HEAD
+        float ycomponent = Mathf.Sin(angle) * force;    
         BulletRB.AddForce(xcomponent, ycomponent, 0f);
-=======
-        BulletRB.AddForce(xcomponent,ycomponent,0f);
-    }
-    float FindTheAngle()
-    {
-        Vector3 P = fireLocation;
-        Vector3 Q = MousePointer.GetHitLocation();
-        float theta = Mathf.Atan((Q.y-P.y)/(Q.x-P.x)) *180/Mathf.PI;
-        if (theta>0 && (Q.y-P.y<0))
-            theta += 180;
-        else if (theta<0 && (Q.y-P.y>0))
-            theta += 180;
-        Debug.Log(theta);
-        return theta;
->>>>>>> Trajectory
     }
 }

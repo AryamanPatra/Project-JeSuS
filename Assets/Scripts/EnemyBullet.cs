ca94fead15af3731e5d9efbf866b722c1e62cc92
Aryaman Patra <aryamanprivate26@gmail.com>
2022-08-15 11:31:46 +0530

    Added EnemyBullet.cs

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    const float DAMAGE = 5.0f;
+
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+    void OnTriggerEnter(Collider cold)
+    {
+        if (cold.gameObject.name=="Toy Player")
+        {
+            Player hit = cold.gameObject.GetComponent<Player>();
+            hit.health -= DAMAGE;
+            hit.healthPlayer.text = hit.health.ToString();
+            if (hit.health<=0)
+                Destroy(cold.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(7);
+            Destroy(gameObject);
+        }
+    }
+}

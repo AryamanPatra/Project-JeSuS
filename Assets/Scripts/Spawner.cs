39849e8289daa2b3db2f1b2df736cc847cbd183e
Aryaman Patra <aryamanprivate26@gmail.com>
2022-08-20 18:14:31 +0530

    Added cloud movements

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,22 +3,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject gamePrefab;
+    public GameObject enemy;
     public float respawnTime = 4.0f;
     private Vector3 screenBounds;
     public static int onScreenEnemy = 0;
-    
+    [SerializeField]
+    GameObject[] cArray;
+    float size;
+    float rotate;
     
     void Start()
     {
         StartCoroutine(ToySpawnTimer());
+        StartCoroutine(SpawnCloudTimer());
     }
-
     private void SpawnEnemy()
     {
         if (onScreenEnemy<3)
         {
-            GameObject a = Instantiate(gamePrefab) as GameObject;
+            GameObject a = Instantiate(enemy) as GameObject;
             onScreenEnemy++;
             a.transform.position = new Vector3(transform.position.x,Random.Range(-2f,6.5f),transform.position.z); 
             a.GetComponent<ToyNPC>().speed = Random.Range(1.0f,4.0f);
@@ -32,5 +35,24 @@ public class Spawner : MonoBehaviour
             yield return new WaitForSecondsRealtime(respawnTime);
             SpawnEnemy();
         }
+    }
+
+    IEnumerator SpawnCloudTimer()
+    {
+        while(true)
+        {
+            float j = Random.Range(100,800);    
+            yield return new WaitForSecondsRealtime(j*0.0005f);
+            SpawnCloud(j);
+        }
+    }
+    private void SpawnCloud(float j)
+    {
+        int i = (int)Random.Range(0,3); 
+        GameObject c = Instantiate(cArray[i]) as GameObject;
+        c.transform.position = new Vector3(transform.position.x+800,-59.5f,j);
+        size = Random.Range(0.5f,1.7f);
+        c.transform.localScale = new Vector3(size,size,size);
+        c.transform.Rotate(0,Random.Range(0,15),0);
     }
 }
